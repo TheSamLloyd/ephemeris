cbc1c4fe126cc3289985878157233a312fb9899c
Sam Lloyd <me@thesamlloyd.com>
2018-11-26 18:28:35 -0500

    uh started working on draft 3 because i don't know how to structure programs

--- Program.cs
+++ Program.cs
@@ -7,17 +7,28 @@ namespace ephemeris
 {
     class Program
     {
-        static void Main(string[] args){
-            System.Console.WriteLine(Ephemeris.ephFile.toJD());
+        static void Main(string[] args)
+        {
+            Ephemeris.ephFile eph = new Ephemeris.ephFile("ascp1950.430.txt", 2433264.5, 2469808.5);
+            eph.getBlock(2);
         }
     }
     class Ephemeris
     {
+        public static double toJD(DateTime dt = new DateTime())
+        {
+            if (dt.Equals(new DateTime()))
+            {
+                dt = DateTime.UtcNow;
+            }
+            double unixEpoch = 2440587.5;
+            double jd = unixEpoch + (dt.Ticks - DateTime.UnixEpoch.Ticks) / (10_000_000.0) / (60 * 60 * 24.0);
+            return jd;
+        }
         public static double parser(string toParse)
         {
             string[] array = toParse.Split("D");
-            double output;
-            double mantissa;
+            double output, mantissa;
             short exponent;
             if (array.Length != 2)
             {
@@ -39,64 +50,67 @@ namespace ephemeris
                 throw new Exception("Couldn't parse input....");
             }
         }
-        class Header
-        {
-            class BodyData
-            {
-                Body body;
-                int StartPoint;
-                int nCoefs;
-                int nSets;
-                public BodyData(Body body, int StartPoint, int nCoefs, int nSets = 1)
-                {
-                    this.body = body;
-                    this.StartPoint = StartPoint;
-                    this.nCoefs = nCoefs;
-                    this.nSets = nSets;
-                }
-            }
-            
-            static Body SolarSystemBarycenter = new Body("Solar System Barycenter");
-            static Body Earth = new Body("Earth", SolarSystemBarycenter);
 
-            BodyData[] bodies = new BodyData[10] {
-                new BodyData(new Body("Mercury", SolarSystemBarycenter), 3, 14, 4),
-                new BodyData(new Body("Venus", SolarSystemBarycenter), 171, 10, 2),
-                new BodyData(new Body("Earth-Moon Barycenter", SolarSystemBarycenter), 231, 13, 2),
-                new BodyData(new Body("Mars", SolarSystemBarycenter), 309, 11),
-                new BodyData(new Body("Jupiter", SolarSystemBarycenter), 342, 8),
-                new BodyData(new Body("Saturn", SolarSystemBarycenter), 366, 7),
-                new BodyData(new Body("Uranus", SolarSystemBarycenter), 387, 6),
-                new BodyData(new Body("Neptune", SolarSystemBarycenter), 405, 6),
-                new BodyData(new Body("Pluto", SolarSystemBarycenter), 423, 6),
-                new BodyData(new Body("Moon", Earth), 441, 13, 8),
+        Body[] bodies = new Body[10] {
+                new Body("Mercury", 3, 14, 4),
+                new Body("Venus", 171, 10, 2),
+                new Body("Earth-Moon Barycenter", 231, 13, 2),
+                new Body("Mars", 309, 11),
+                new Body("Jupiter", 342, 8),
+                new Body("Saturn", 366, 7),
+                new Body("Uranus", 387, 6),
+                new Body("Neptune", 405, 6),
+                new Body("Pluto", 423, 6),
+                new Body("Moon", 441, 13, 8),
 
             };
+        class Header
+        {
+            //need to figure out: which file to use, which block to take. We can... do that.
+
         }
         public class ephFile
         {
-            int startYear;
-            int endYear;
-
+            double startTime, endTime;
+            string filename;
             //some magic numbers here
             int linesPerBlock = 341;
             int numbersPerLine = 3;
             int blocksPerFile = 1142;
+            int dimension = 3;
 
-            public static double toJD(DateTime dt = new DateTime()){
-                if (dt.Equals(new DateTime())){
-                    dt = DateTime.UtcNow;
-                }
-                double unixEpoch = 2440587.5;
-                double jd = unixEpoch + (dt.Ticks - DateTime.UnixEpoch.Ticks)/(10_000_000.0) / (60 * 60 * 24.0);
-                return jd;
+            public ephFile(string filename, double startTime, double endTime)
+            {
+                this.filename = filename;
+                this.startTime = startTime;
+                this.endTime = endTime;
             }
 
-            class Block
+            public Block getBlock(int blockNumber)
             {
-                Block(double[] data)
+                double[] blockData = new double[341 * 3];
+                IEnumerable<string> rawData = fileReader.getLines(filename, blockNumber * linesPerBlock, linesPerBlock);
+                int i = 0;
+                foreach (string line in rawData)
                 {
-
+                    foreach (string entry in line.Split(" ", System.StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        blockData[i] = Ephemeris.parser(entry);
+                        i++;
+                    }
+                }
+                return new Block(blockData);
+            }
+            public class Block
+            {
+                double[] data;
+                double startTime;
+                double endTime;
+                public Block(double[] data)
+                {
+                    this.data = data;
+                    this.startTime = data[0];
+                    this.endTime = data[1];
                 }
                 class Coefficients
                 {
@@ -125,30 +139,52 @@ namespace ephemeris
         {
             string name;
             Body relativeTo;
-            class Coordinate
+            public class Coordinate
             {
                 double[] coordinate = new double[3];
-                double x => this.coordinate[0];
-                double y => this.coordinate[1];
-                double z => this.coordinate[2];
+                public double x => this.coordinate[0];
+                public double y => this.coordinate[1];
+                public double z => this.coordinate[2];
+                public Coordinate(double x=0, double y=0, double z=0){
+                    this.coordinate = new double[3] {x,y,z};
+                }
+                public Coordinate(double[] coords){
+                    this.coordinate = coords;
+                }
             }
             Coordinate position;
-            Coordinate velocity;
 
-            public Body(string name, Body relativeTo = null)
+            public Coordinate getPosition(){
+                return this.position;
+            }
+            Coordinate velocity;
+            public Coordinate getVelocity(){
+                return this.velocity;
+            }
+            
+            public Body(string name, int StartPoint, int nCoefs, int nSets = 1)
             {
                 this.name = name;
-                this.relativeTo = relativeTo;
+
+            }
+            Coordinate correctRelative(Body satellite, Body primary)
+            {
+                double x,y,z;
+                x=satellite.getPosition().x-primary.getPosition().x;
+                y=satellite.getPosition().y-primary.getPosition().y;
+                z=satellite.getPosition().z-primary.getPosition().z;
+
+                return new Coordinate(x,y,z);
             }
         }
     }
 
-    static class fileReader
+    public static class fileReader
     {
-        static IEnumerable<String> getLines(string path, int lineStart = 0, int lineEnd = -1)
+        public static IEnumerable<String> getLines(string path, int lineStart = 0, int count = 0)
         {
 
-            return System.IO.File.ReadLines(path).TakeWhile((line, index) => index >= lineStart & (index <= lineEnd | lineEnd == -1));
+            return System.IO.File.ReadLines(path).Skip(lineStart).Take(count);
 
         }
     }
