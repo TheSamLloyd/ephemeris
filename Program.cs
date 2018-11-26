2dcbcb80f208cbd0675206a02c8475a050297a4d
Sam Lloyd <me@thesamlloyd.com>
2018-11-26 13:43:25 -0500

    added convert to JD function, etc

--- Program.cs
+++ Program.cs
@@ -7,7 +7,9 @@ namespace ephemeris
 {
     class Program
     {
-        static void Main(string[] args) => System.Console.WriteLine("Nothing implemented yet.");
+        static void Main(string[] args){
+            System.Console.WriteLine(Ephemeris.ephFile.toJD());
+        }
     }
     class Ephemeris
     {
@@ -30,8 +32,7 @@ namespace ephemeris
             }
             else if (Double.TryParse(array[0], out mantissa) && Int16.TryParse(array[1], out exponent))
             {
-                output = mantissa * Math.Pow(10.0, exponent);
-                return output;
+                return mantissa * Math.Pow(10.0, exponent);
             }
             else
             {
@@ -40,12 +41,57 @@ namespace ephemeris
         }
         class Header
         {
+            class BodyData
+            {
+                Body body;
+                int StartPoint;
+                int nCoefs;
+                int nSets;
+                public BodyData(Body body, int StartPoint, int nCoefs, int nSets = 1)
+                {
+                    this.body = body;
+                    this.StartPoint = StartPoint;
+                    this.nCoefs = nCoefs;
+                    this.nSets = nSets;
+                }
+            }
+            
+            static Body SolarSystemBarycenter = new Body("Solar System Barycenter");
+            static Body Earth = new Body("Earth", SolarSystemBarycenter);
 
+            BodyData[] bodies = new BodyData[10] {
+                new BodyData(new Body("Mercury", SolarSystemBarycenter), 3, 14, 4),
+                new BodyData(new Body("Venus", SolarSystemBarycenter), 171, 10, 2),
+                new BodyData(new Body("Earth-Moon Barycenter", SolarSystemBarycenter), 231, 13, 2),
+                new BodyData(new Body("Mars", SolarSystemBarycenter), 309, 11),
+                new BodyData(new Body("Jupiter", SolarSystemBarycenter), 342, 8),
+                new BodyData(new Body("Saturn", SolarSystemBarycenter), 366, 7),
+                new BodyData(new Body("Uranus", SolarSystemBarycenter), 387, 6),
+                new BodyData(new Body("Neptune", SolarSystemBarycenter), 405, 6),
+                new BodyData(new Body("Pluto", SolarSystemBarycenter), 423, 6),
+                new BodyData(new Body("Moon", Earth), 441, 13, 8),
+
+            };
         }
-        class ephFile
+        public class ephFile
         {
             int startYear;
             int endYear;
+
+            //some magic numbers here
+            int linesPerBlock = 341;
+            int numbersPerLine = 3;
+            int blocksPerFile = 1142;
+
+            public static double toJD(DateTime dt = new DateTime()){
+                if (dt.Equals(new DateTime())){
+                    dt = DateTime.UtcNow;
+                }
+                double unixEpoch = 2440587.5;
+                double jd = unixEpoch + (dt.Ticks - DateTime.UnixEpoch.Ticks)/(10_000_000.0) / (60 * 60 * 24.0);
+                return jd;
+            }
+
             class Block
             {
                 Block(double[] data)
@@ -62,7 +108,7 @@ namespace ephemeris
                     {
                         private static double N(int n, double x)
                         {
-                            if (-1 <= x & 1 >= x)
+                            if (Math.Abs(x) <= 1)
                             {
                                 return Math.Cos(n * Math.Acos(x));
                             }
@@ -78,16 +124,22 @@ namespace ephemeris
         class Body
         {
             string name;
+            Body relativeTo;
             class Coordinate
             {
                 double[] coordinate = new double[3];
-                Body relativeTo;
                 double x => this.coordinate[0];
                 double y => this.coordinate[1];
                 double z => this.coordinate[2];
             }
             Coordinate position;
             Coordinate velocity;
+
+            public Body(string name, Body relativeTo = null)
+            {
+                this.name = name;
+                this.relativeTo = relativeTo;
+            }
         }
     }
 
@@ -95,15 +147,9 @@ namespace ephemeris
     {
         static IEnumerable<String> getLines(string path, int lineStart = 0, int lineEnd = -1)
         {
-            if (lineEnd == -1)
-            {
-                return System.IO.File.ReadLines(path).TakeWhile((line, index) => index >= lineStart);
-            }
-            else
-            {
-                return System.IO.File.ReadLines(path).TakeWhile((line, index) => index >= lineStart & index <= lineEnd);
-            }
-        }
 
+            return System.IO.File.ReadLines(path).TakeWhile((line, index) => index >= lineStart & (index <= lineEnd | lineEnd == -1));
+
+        }
     }
 }
